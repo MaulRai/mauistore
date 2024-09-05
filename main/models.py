0a7be763f4b886246467f8f6b7e79897ce93c988
MaulRai <muhammadraihanofc@gmail.com>
2024-09-05 10:47:27 +0700

    hias html, css ada tapi blom dimigrate semua, js blom ada

--- main/models.py
+++ main/models.py
@@ -9,3 +9,13 @@ class MoodEntry(models.Model):
     @property
     def is_mood_strong(self):
         return self.mood_intensity > 5
+    
+    
+class Product(models.Model):
+    name = models.CharField(max_length=63)
+    price = models.IntegerField()
+    desc = models.CharField(max_length=255)
+    
+    @property
+    def get_formatted_price(self):
+        return f"Rp{self.price:,.0f}".replace(",", ".")
--- main/views.py
+++ main/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 
+# db_temp = [
+#     ('plushie1', 'Hitagi Plushie', '400000', 'Koyo koyooo'),
+#     ('plushie2', 'Suruga Plushie', '400000', 'I love my senpai'),
+#     ('plushie3', 'Karen Plushie', '500000', 'Asta la vistaaa'),
+#     ('plushie4', 'Hanekawa Plushie', '550000', 'i don\'t know everything i just know what i know'),
+#     ('plushie5', 'Shinobu Plushie', '450000', 'ka..ka.. >_<'),
+#     ('plushie6', 'Nadeko Plushie', '400000', 'Koyomi-onichan'),
+#     ('plushie7', 'Mayoi Plushie', '450000', 'Sorry I stuttered, Arararagi-san'),
+# ]
+
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306216636',
+        'name': 'Muhammad Raihan Maulana',
+        'class': 'PBP D'
+    }
+
+    return render(request, "main.html", context)
+
+def show_products(request, index):
+    context = {
+        'image_url' : db_temp[index][0],
+        'name' : db_temp[index][1],
+        'price' : db_temp[index][2],
+        'desc' : db_temp[index][3],
     }
 
     return render(request, "main.html", context)
